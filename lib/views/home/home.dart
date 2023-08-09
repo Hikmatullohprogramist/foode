b674d83f9875f65d695b35b1c7aa0782c18053ae
OSIYOCOMPUTERS™ <oyatillo7777@gmail.com>
2023-08-09 15:49:45 +0500

    oling2

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -130,41 +130,167 @@ class _HomeScreenState extends State<HomeScreen> {
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(40),
                     color: Colors.green[400]),
-                child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 30.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "30%",
-                              style: TextStyle(
-                                  fontSize: 50,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w900),
-                            ),
-                            Text(
-                              "DISCOUNT ONLY",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w900,),
-                            ),
-                            Text(
-                              "VALID FOR TODAY!",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w900),
-                            ),
-                          ],
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "30%",
+                          style: TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900),
                         ),
-                      ),
-                      Expanded(child: Image(image: AssetImage("assets/home/img_1.png"),))]),
+                        Text(
+                          "DISCOUNT ONLY",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Text(
+                          "VALID FOR TODAY!",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Image(
+                    image: AssetImage("assets/home/img_1.png"),
+                  ))
+                ]),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_2.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Hambur..",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_3.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Pizza",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_4.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Noodles",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_5.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Meat",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_6.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Vegeta..",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_7.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Disert",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_8.png"),
+                            width: 60,
+                            height: 70),
+                        Text("Drink",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 80,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Image.asset("assets/home/img_9.png"),
+                            width: 60,
+                            height: 70),
+                        Text("More",style: TextStyle(fontSize: 16),)
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
           ],
         ),
       ),
